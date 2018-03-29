e4a3a758ab5029802b131808fa4855d1e0b94f4b
rupali.meshram <meshram.rupali86@gmail.com>
2018-03-29 11:14:39 -0400

    burger-shop

--- db/schema.sql
+++ db/schema.sql
@@ -10,6 +10,6 @@ CREATE TABLE burger
 	id int NOT NULL AUTO_INCREMENT,
 	burger_name varchar(255) NOT NULL,
 	devoured BOOLEAN DEFAULT true,
-	`date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	
 	PRIMARY KEY (id)
 );
